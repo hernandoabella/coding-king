368c1da270798305df236cba571c603a56216790
Hernando Abella <hernandoabella@gmail.com>
2024-04-19 21:19:40 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <div className="container-fluid">
+      <div className="container-fluid p-8">
         <p className="text-center">
           &copy; Coding-King {new Date().getFullYear()}.
         </p>
