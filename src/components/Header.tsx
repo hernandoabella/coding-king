680aeab0bb65f2c43c5607f3e3d15f32db068596
Hernando Abella <hernandoabella@gmail.com>
2024-04-19 21:19:41 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header className="p-8">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <h1 className="text-center italic">Coding-King 🤴</h1>
-          </a>
-        </div>
-      </nav>
+      <h1 className="text-center italic">Coding-King 🤴</h1>
     </header>
   );
 }
