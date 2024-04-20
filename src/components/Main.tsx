115d4b0ad21c5572fc630bb4b78d240a1a314d43
Hernando Abella <hernandoabella@gmail.com>
2024-04-19 21:19:42 -0500

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -10,19 +10,19 @@ const links = [
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5, ease: "easeInOut" }}
       >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sint
-        minima itaque, distinctio quasi quia, harum veniam quidem nam voluptas
-        sapiente ad facere perspiciatis eveniet dicta enim, mollitia eaque.
+        The foundation of web development, HTML is used to structure the content
+        of a webpage.
       </motion.p>
     ),
     element: (
-      <motion.div
+      <motion.img
+        src="/public/html5-original.svg"
+        alt="css3"
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5, ease: "easeInOut" }}
-      >
-        Example Div
-      </motion.div>
+        width={400}
+      />
     ),
   },
   {
@@ -33,18 +33,18 @@ const links = [
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5, ease: "easeInOut" }}
       >
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat
-        voluptates facere temporibus, itaque officiis accusantium laboriosam
-        repellendus, autem doloremque in.
+        Cascading Style Sheets (CSS) is a stylesheet language used for
+        describing the look and formatting of a web page.
       </motion.p>
     ),
     element: (
       <motion.img
-        src="example-image2.jpg"
-        alt="Example Image"
+        src="/public/css3-original.svg"
+        alt="css3"
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5, ease: "easeInOut" }}
+        width={400}
       />
     ),
   },
@@ -56,27 +56,30 @@ function Main() {
 
   return (
     <div>
-      <div className="flex justify-center place-items-center p-5 gap-10">
-        <div className="md:w-1/2">
-          {/* Left side content example: code snippet or image */}
-          {links[activeLink].element}
-        </div>
-        <div className="md:w-1/2">
-          <motion.h2
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, ease: "easeInOut" }}
-          >
-            {links[activeLink].title}
-          </motion.h2>
-          {links[activeLink].content}
-          <a href="#">Read more.</a>
+      <div className="md:w-2xl md:mx-auto flex justify-center place-items-center p-5 gap-10">
+        <div className="md:flex">
+          <div className="md:w-1/3">
+            {/* Left side content example: code snippet or image */}
+            {links[activeLink].element}
+          </div>
+          <div className="md:w-1/2">
+            <motion.h2
+              className="font-semibold"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            >
+              {links[activeLink].title}
+            </motion.h2>
+            {links[activeLink].content}
+            <a href="#">Read more.</a>
+          </div>
         </div>
       </div>
-      <div className="flex p-5 gap-5 border ">
+      <div className="flex p-5 gap-5 border text-center justify-center place-items-center">
         {links.map((link, index) => (
           <div
-            className="bg-yellow"
+            className="bg-indigo-500 hover:bg-indigo-300 hover:text-black text-white rounded-full p-2"
             key={index}
             onClick={() => setActiveLink(index)}
             style={{ cursor: "pointer" }}
