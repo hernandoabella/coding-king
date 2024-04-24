d2b5832fc0dac5258f0ec4b3c902adf18b58ffea
Hernando Abella <hernandoabella@gmail.com>
2024-04-24 15:55:44 -0500

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,46 +6,13 @@ const links = [
     title: "HTML",
     content: (
       <div>
-        <p>
+        <p className="py-2">
           The foundation of web development, HTML is used to structure the
           content of a webpage.
         </p>
-        <ul>
-          <li>
-            <b>!DOCTYPE: </b>Defines the document
-          </li>
-          <li>
-            <b>html: </b>Defines HTML documents
-          </li>
-          <li>
-            <b>head: </b>Contains metadata / information for the document
-          </li>
-          <li>
-            <b>title</b> Defines the title for the document
-          </li>
-          <li>
-            <b>body:</b> Defines the document
-          </li>
-          <li>
-            <b>h1 - h6:</b> Defines headings
-          </li>
-          <li>
-            <b>p:</b> Defines a paragraph
-          </li>
-          <li>
-            <b>br:</b> Insert a single line break
-          </li>
-          <li>
-            <b>hr:</b> Defines a thematic break in an HTML page
-          </li>
-          <li>
-            <b>:</b>Defines a comment
-          </li>
-        </ul>
-        <h3 className="font-semibold uppercase">Forms and inputs</h3>
-        <h3 className="font-semibold">FORMATTING</h3>
-        <h3 className="font-semibold">IMAGES</h3> <br />
-        <h3 className="font-semibold uppercase">Basic HTML</h3>
+        <div className="py-5">
+          <a href="#" className="bg-blue-100 rounded-xl p-1 block w-full text-center w-1/5 pointer">reference1</a>
+        </div>
       </div>
     ),
     element: (
@@ -63,16 +30,21 @@ const links = [
   {
     title: "CSS",
     content: (
-      <p>
-        Cascading Style Sheets (CSS) is a stylesheet language used for
-        describing the look and formatting of a web page.
-      </p>
+      <div>
+        <p className="py-2">
+          Cascading Style Sheets (CSS) is a stylesheet language used for
+          describing the look and formatting of a web page.
+        </p>
+        <div className="py-5">
+          <a href="#" className="bg-blue-100 rounded-xl p-1 block w-full text-center w-1/5 pointer">reference1</a>
+        </div>
+      </div>
     ),
     element: (
       <motion.img
-        key="css-image"
+        key="html-image"
         src="/public/css3-original.svg"
-        alt="CSS3"
+        alt="HTML5"
         width={400}
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
@@ -87,25 +59,26 @@ function Main() {
 
   return (
     <div>
-      <div className="md:w-2xl md:mx-auto flex justify-center place-items-center p-10 gap-10">
-        <div className="md:flex gap-10">
-          <div className="md:p-10 mx-auto m-10 w-1/2">
+      <div className="flex align-center items-center bg-red-500 md:mx-auto p-10">
+        <div className="md:flex gap-10 justify-center items-center">
+          <div className="md:w-2/1 flex justify-center items-center p-10">
             <motion.div>{links[activeLink].element}</motion.div>
           </div>
-          <div>
-            <motion.h2 className="font-semibold  md:text-7xl">
+          <div className="md:w-2/1 p-10">
+            <motion.h2 className="font-semibold text-indigo-700 md:text-7xl">
               {links[activeLink].title}
             </motion.h2>
             {links[activeLink].content}
           </div>
         </div>
+
+
       </div>
-      <div className="flex p-5 gap-5 text-center justify-center place-items-center">
+      <div className="flex justify-center p-10 gap-5 text-center">
         {links.map((link, index) => (
           <div
-            className={`bg-indigo-500 hover:bg-indigo-300 hover:text-black text-white rounded-full p-2 ${
-              index === activeLink && "bg-gray-700"
-            }`}
+            className={`bg-indigo-500 hover:bg-indigo-700 text-white rounded-full p-2 ${index === activeLink && "bg-gray-700"
+              }`}
             key={index}
             onClick={() => setActiveLink(index)}
             style={{ cursor: "pointer" }}
@@ -115,7 +88,9 @@ function Main() {
         ))}
       </div>
     </div>
+
   );
 }
+
 
 export default Main;
