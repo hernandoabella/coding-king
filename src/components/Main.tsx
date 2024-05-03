115e6b2140587e151f731d1befe1287ff5ab79f5
Hernando Abella <hernandoabella@gmail.com>
2024-05-02 23:37:14 -0500

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,23 +4,40 @@ import { motion } from "framer-motion";
 function Main() {
   return (
     <div className="container">
-      <div>
+      <div className="p-10 flex">
         <div>
-          <img src="/public/html5-original.svg" width={300} height={300} alt="html"/>
+          <img
+            src="/public/html5-original.svg"
+            width={300}
+            height={300}
+            alt="html"
+          />
         </div>
-        <p className="py-2 text-xl">
-          The foundation of web development, HTML is used to structure the
-          content of a webpage.
-        </p>
-        <div className="py-5">
-          <a
-            href="#"
-            className="bg-blue-100 rounded-xl p-1 block text-center w-1/5 pointer"
-          >
-            link
-          </a>
+        <div>
+          <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+              HTML:
+            </span>{" "}
+            HyperText Markup Language
+          </h1>
+          <p className="py-2 text-xl">
+            The foundation of web development, HTML is used to structure the
+            content of a webpage.
+          </p>
+          <div className="py-5">
+            <a href="#">
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Link
+              </button>
+            </a>
+          </div>
         </div>
       </div>
+
+      <div>Navigation</div>
     </div>
   );
 }
