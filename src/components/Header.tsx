f82abb53f14f1dd991dca024ad831c045175ad73
Hernando Abella <hernandoabella@gmail.com>
2024-05-02 22:35:04 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,11 @@ import React from "react";
 function Header() {
   return (
     <header className="p-10">
-      <h1 className="text-center italic font-semibold">Coding-King 🤴</h1>
+      <h1 className="text-center font-bold">Coding-King 🤴</h1>
+      <p className="text-center">
+        Quick resources to help you reach the highest level in just the blink of
+        an eye.
+      </p>
     </header>
   );
 }
