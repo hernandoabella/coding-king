170332e3f4a9a4775c8c8276375e67c505ef1c44
Hernando Abella <hernandoabella@gmail.com>
2024-05-02 23:44:05 -0500

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -24,13 +24,14 @@ function Main() {
             The foundation of web development, HTML is used to structure the
             content of a webpage.
           </p>
+          
           <div className="py-5">
             <a href="#">
               <button
                 type="button"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                Link
+                Read more
               </button>
             </a>
           </div>
