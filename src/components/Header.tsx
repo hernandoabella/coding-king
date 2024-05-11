4b3c26e998bccbf3696e519fa16c148c666e49b8
Hernando Abella <hernandoabella@gmail.com>
2024-05-11 01:25:02 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,128 +18,89 @@ function Header() {
   };
 
   return (
-    <header className="p-10 md:flex justify-center items-center">
+    <header className="pb-5">
       <div>
-        <h1 className="text-center font-bold">Coding-King 🤴</h1>
-        <p className="text-center">
+        <div className="dropdown mb-5">
+          <div tabIndex={0} role="button" className="btn m-1">
+            Theme
+            <svg
+              width="12px"
+              height="12px"
+              className="h-2 w-2 fill-current opacity-60 inline-block"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 2048 2048"
+            >
+              <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52"
+          >
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                aria-label="Default"
+                value="default"
+                checked={selectedTheme === "default"}
+                onChange={handleThemeChange}
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                aria-label="Retro"
+                value="retro"
+                checked={selectedTheme === "retro"}
+                onChange={handleThemeChange}
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                aria-label="Cyberpunk"
+                value="cyberpunk"
+                checked={selectedTheme === "cyberpunk"}
+                onChange={handleThemeChange}
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                aria-label="Valentine"
+                value="Valentine"
+                checked={selectedTheme === "Valentine"}
+                onChange={handleThemeChange}
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                aria-label="Aqua"
+                value="aqua"
+                checked={selectedTheme === "aqua"}
+                onChange={handleThemeChange}
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h1 className="font-bold">Coding-King 🤴</h1>
+        <p>
           Quick resources to help you reach the highest level in just the blink
           of an eye.
         </p>
-      </div>
-      <div className="flex gap-5 items-center absolute right-40">
-        <div className="">
-          <a
-            href="https://github.com/hernandoabella/coding-king"
-            aria-label="github project"
-            target="_blank"
-          >
-            <svg
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" />
-            </svg>
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://x.com/codingkingx/"
-            aria-label="github project"
-            target="_blank"
-          >
-            <svg
-              className="w-6 h-6"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M13.795 10.533 20.68 2h-3.073l-5.255 6.517L7.69 2H1l7.806 10.91L1.47 22h3.074l5.705-7.07L15.31 22H22l-8.205-11.467Zm-2.38 2.95L9.97 11.464 4.36 3.627h2.31l4.528 6.317 1.443 2.02 6.018 8.409h-2.31l-4.934-6.89Z" />
-            </svg>
-          </a>
-        </div>
-        <div>
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn m-1">
-              Theme
-              <svg
-                width="12px"
-                height="12px"
-                className="h-2 w-2 fill-current opacity-60 inline-block"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 2048 2048"
-              >
-                <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52"
-            >
-              <li>
-                <input
-                  type="radio"
-                  name="theme-dropdown"
-                  className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                  aria-label="Default"
-                  value="default"
-                  checked={selectedTheme === "default"}
-                  onChange={handleThemeChange}
-                />
-              </li>
-              <li>
-                <input
-                  type="radio"
-                  name="theme-dropdown"
-                  className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                  aria-label="Retro"
-                  value="retro"
-                  checked={selectedTheme === "retro"}
-                  onChange={handleThemeChange}
-                />
-              </li>
-              <li>
-                <input
-                  type="radio"
-                  name="theme-dropdown"
-                  className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                  aria-label="Cyberpunk"
-                  value="cyberpunk"
-                  checked={selectedTheme === "cyberpunk"}
-                  onChange={handleThemeChange}
-                />
-              </li>
-              <li>
-                <input
-                  type="radio"
-                  name="theme-dropdown"
-                  className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                  aria-label="Valentine"
-                  value="Valentine"
-                  checked={selectedTheme === "Valentine"}
-                  onChange={handleThemeChange}
-                />
-              </li>
-              <li>
-                <input
-                  type="radio"
-                  name="theme-dropdown"
-                  className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                  aria-label="Aqua"
-                  value="aqua"
-                  checked={selectedTheme === "aqua"}
-                  onChange={handleThemeChange}
-                />
-              </li>
-            </ul>
-          </div>
-        </div>
       </div>
     </header>
   );
