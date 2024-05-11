1a1cf66605fb208bffd615127eca2469b3a23ffe
Hernando Abella <hernandoabella@gmail.com>
2024-05-11 02:10:45 -0500

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import HTML from "./HTML";
+import CSS from "./CSS";
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -15,6 +16,11 @@ function Main() {
     { label: "HTML", icon: "devicon-html5-plain", type: "HTML" },
     { label: "CSS", icon: "devicon-css3-plain", type: "CSS" },
     {
+      label: "Tailwind",
+      icon: "devicon-tailwindcss-plain",
+      type: "JavaScript",
+    },
+    {
       label: "JavaScript",
       icon: "devicon-javascript-plain",
       type: "JavaScript",
@@ -24,61 +30,44 @@ function Main() {
       icon: "devicon-typescript-plain",
       type: "JavaScript",
     },
-    {
-      label: "Tailwind",
-      icon: "devicon-tailwindcss-plain",
-      type: "JavaScript",
-    },
   ];
 
   return (
-    <div>
-      <div className="flex">
-        <div className="p-5">
-          <Header />
-          <ul className="flex flex-wrap gap-5">
-            {buttonsData.map((button, index) => (
-              <li key={index}>
-                <button
-                  className={`btn btn-outline ${
-                    activeButton === button.type ? "active" : ""
-                  }`}
-                  onClick={() => handleButtonClick(button.type)}
-                >
-                  <i className={button.icon}></i>
-                  {button.label}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div className="flex h-screen justify-center align-">
+      <div className="h-full p-10">
+        <Header />
+        <ul className="flex flex-wrap gap-5">
+          {buttonsData.map((button, index) => (
+            <li key={index}>
+              <button
+                className={`btn btn-outline ${
+                  activeButton === button.type ? "active" : ""
+                }`}
+                onClick={() => handleButtonClick(button.type)}
+              >
+                <i className={button.icon}></i>
+                {button.label}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <Footer />
+      </div>
 
-        <div className="p-10 md:flex">
-          {/* Render content based on activeButton */}
-          {activeButton === "HTML" && <HTML />}
-          {activeButton === "CSS" && (
-            <div className="p-10 flex md:flex-none justify-center">
-              <img
-                src="/css3-original.svg"
-                width={300}
-                height={300}
-                alt="css"
-              />
-            </div>
-          )}
-          {activeButton === "JavaScript" && (
-            <div className="p-10 flex md:flex-none justify-center">
-              <img
-                src="/javascript-original.svg"
-                width={300}
-                height={300}
-                alt="JavaScript"
-              />
-            </div>
-          )}
-          
-        </div>
-        
+      <div className="p-10 md:flex">
+        {/* Render content based on activeButton */}
+        {activeButton === "HTML" && <HTML />}
+        {activeButton === "CSS" && <CSS />}
+        {activeButton === "JavaScript" && (
+          <div className="p-10 flex md:flex-none justify-center">
+            <img
+              src="/javascript-original.svg"
+              width={300}
+              height={300}
+              alt="JavaScript"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
