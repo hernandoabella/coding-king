1ca4bafd6b19aa3eb2cba11bd49d6db7d6bbea9b
Hernando Abella <hernandoabella@gmail.com>
2024-05-14 23:36:40 -0500

    Update ImportUI.tsx

--- src/components/ImportUI.tsx
+++ src/components/ImportUI.tsx
@@ -6,8 +6,10 @@ import Reactd from "./ui/Reactd";
 import Vue from "./ui/Vue";
 import Angular from "./ui/Angular";
 import Git from "./ui/Git";
+import Github from "./ui/Github";
+import Python from "./ui/Python";
+import Java from "./ui/Java";
+import Cpp from "./ui/Cpp";
+import Csharp from "./ui/Csharp";
 
-export { HTML, CSS, JavaScript, TypeScript, Reactd, Vue, Angular, Git };
-
-
-
+export { HTML, CSS, JavaScript, TypeScript, Reactd, Vue, Angular, Git, Github };
