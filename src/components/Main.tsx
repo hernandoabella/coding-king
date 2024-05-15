f5affcd93b861a8ed6e2727ca1c6607ad46fab46
Hernando Abella <hernandoabella@gmail.com>
2024-05-14 23:08:51 -0500

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { HTML, CSS, Git } from "./ImportUI";
+import { buttonsData } from "./buttonsData";
 
 function Main() {
   const [activeButton, setActiveButton] = useState("");
@@ -29,86 +30,12 @@ function Main() {
     setActiveButton(buttonType);
   };
 
-  const buttonsData = [
-    {
-      category: "Web Development",
-      buttons: [
-        { label: "HTML", icon: "devicon-html5-plain", type: "HTML" },
-        { label: "CSS", icon: "devicon-css3-plain", type: "CSS" },
-        {
-          label: "JavaScript",
-          icon: "devicon-javascript-plain",
-          type: "JavaScript",
-        },
-        {
-          label: "TypeScript",
-          icon: "devicon-typescript-plain",
-          type: "TypeScript",
-        },
-        { label: "React", icon: "devicon-react-plain", type: "React" },
-        { label: "Vue", icon: "devicon-vuejs-plain", type: "Vue" },
-        { label: "Angular", icon: "devicon-angular-plain", type: "Angular" },
-      ],
-    },
-    {
-      category: "Control Version",
-      buttons: [
-        { label: "Git", icon: "devicon-git-plain", type: "Git" },
-        { label: "Github", icon: "devicon-github-plain", type: "Github" },
-      ],
-    },
-    {
-      category: "Programming Languages",
-      buttons: [
-        { label: "Python", icon: "devicon-python-plain", type: "Python" },
-        { label: "Java", icon: "devicon-java-plain", type: "Java" },
-        { label: "C++", icon: "devicon-cplusplus-plain", type: "C++" },
-        { label: "C#", icon: "devicon-csharp-plain", type: "C#" },
-        { label: "Swift", icon: "devicon-swift-plain", type: "Swift" },
-        { label: "Kotlin", icon: "devicon-kotlin-plain", type: "Kotlin" },
-      ],
-    },
-    {
-      category: "Databases",
-      buttons: [
-        { label: "MySQL", icon: "devicon-mysql-plain", type: "Mysql" },
-        {
-          label: "PostgresSQL",
-          icon: "devicon-postgresql-plain",
-          type: "PostgresSQL",
-        },
-        { label: "MongoDB", icon: "devicon-mongodb-plain", type: "MongoDB" },
-        { label: "Redis", icon: "devicon-redis-plain", type: "Github" },
-      ],
-    },
-    {
-      category: "Cloud Computing",
-      buttons: [
-        { label: "AWS", icon: "devicon-amazonwebservices-plain", type: "AWS" },
-        { label: "Azure", icon: "devicon-azure-plain", type: "Azure" },
-        {
-          label: "Google Cloud",
-          icon: "devicon-googlecloud-plain",
-          type: "Google Cloud",
-        },
-      ],
-    },
-    {
-      category: "Mobile Development",
-      buttons: [
-        { label: "Android", icon: "devicon-android-plain", type: "Android" },
-        { label: "XCode", icon: "devicon-xcode-plain", type: "XCODE" },
-        { label: "Flutter", icon: "devicon-flutter-plain", type: "Flutter" },
-      ],
-    },
-  ];
-
   return (
-    <div className="flex h-screen justify-center">
-      <div className="p-10">
+    <div className="flex h-screen w-screen overflow-hidden justify-center items-center">
+      <div className="">
         <Header />
         <div className="">
-          <div className="space-x-5">
+          <div className="flex flex-wrap gap-5">
             {buttonsData.map((categoryData, index) => (
               <button
                 key={index}
