02bbe775ebee3b157d3af2913d1a1c21b6356f0c
Hernando Abella <hernandoabella@gmail.com>
2024-05-14 23:34:03 -0500

    Update ImportUI.tsx

--- src/components/ImportUI.tsx
+++ src/components/ImportUI.tsx
@@ -1,5 +1,13 @@
 import HTML from "./ui/HTML";
 import CSS from "./ui/CSS";
+import JavaScript from "./ui/JavaScript";
+import TypeScript from "./ui/TypeScript";
+import Reactd from "./ui/Reactd";
+import Vue from "./ui/Vue";
+import Angular from "./ui/Angular";
 import Git from "./ui/Git";
-export { HTML, CSS, Git };
+
+export { HTML, CSS, JavaScript, TypeScript, Reactd, Vue, Angular, Git };
+
+
 
