fcf0efb29bf96ec6569a60e31bb421e70cb85081
Hernando Abella <hernandoabella@gmail.com>
2024-05-16 01:19:33 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'verdana': ['Verdana', 'sans-serif']
+      }
+    },
   },
   plugins: [
     require("daisyui")
