c4a0215c6c538a4f6644d69197b600c7c6efae0d
Hernando Abella <hernandoabella@gmail.com>
2024-05-15 23:26:44 -0500

    Update ImportUI.tsx

--- src/components/ImportUI.tsx
+++ src/components/ImportUI.tsx
@@ -11,6 +11,31 @@ import Python from "./ui/Python";
 import Java from "./ui/Java";
 import Cpp from "./ui/Cpp";
 import Csharp from "./ui/Csharp";
+import Swift from "./ui/Swift";
+import Kotlin from "./ui/Kotlin";
+import Mysql from "./ui/Mysql";
+import Postgresql from "./ui/Postgresql";
+import Mongodb from "./ui/Mongodb";
+import Redis from "./ui/Redis";
+import AWS from "./ui/aws";
+import Azure from "./ui/microsoft-azure";
+import GoogleCloud from "./ui/google-cloud-platform";
+import Android from "./ui/android";
+import Xcode from "./ui/Xcode";
+import Flutter from "./ui/Flutter";
 
-
-export { HTML, CSS, JavaScript, TypeScript, Reactd, Vue, Angular, Git, Github };
+export {
+  HTML,
+  CSS,
+  JavaScript,
+  TypeScript,
+  Reactd,
+  Vue,
+  Angular,
+  Git,
+  Github,
+  Python,
+  Java,
+  Cpp,
+  Csharp,
+};
