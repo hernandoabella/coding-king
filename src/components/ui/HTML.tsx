c5b98979a476bc95f8d515ef95647407d4fcaeab
Hernando Abella <hernandoabella@gmail.com>
2024-07-09 09:01:15 -0500

    Update HTML.tsx

--- src/components/ui/HTML.tsx
+++ src/components/ui/HTML.tsx
@@ -16,9 +16,9 @@ function Html() {
       exit={{ opacity: 0, y: -50 }} // Animation when component unmounts
       transition={{ duration: 0.5 }} // Animation duration
     >
-      <div className="flex justify-center">
+      <div className="flex flex-col md:flex-row justify-center">
         <div className=" items-center">
-          <div className="flex gap-5">
+          <div className="flex flex-col items-center md:flex-row gap-5">
             <div className="">
               <img
                 src="/html5-original.svg"
@@ -28,7 +28,7 @@ function Html() {
               />
             </div>
             <div>
-              <h1 className="mb-4 text-3xl font-extrabold md:text-5xl lg:text-4xl">
+              <h1 className="italic mb-4 text-3xl font-extrabold md:text-5xl lg:text-4xl">
                 <span className="text-transparent bg-clip-text bg-gradient-to-r to-orange-600 from-orange-400">
                   HTML:
                 </span>{" "}
@@ -63,14 +63,14 @@ function Html() {
             </div>
           </div>
 
-          <div className="pt-5 gap-8 md:flex">
+          <div className="pt-5 gap-8 flex flex-col md:flex-row justify-center">
             <motion.div
               initial={{ opacity: 0, x: -50 }} // Initial animation state
               animate={{ opacity: 1, x: 0 }} // Animation when component mounts
               transition={{ delay: 0.4 }} // Delay before animation starts
             >
-              <h3 className="text-2xl font-bold">Basic Structure:</h3>
-              <p className="text-gray-600">
+              <h3 className="text-2xl font-bold py-2">Basic Structure:</h3>
+              <p className="pb-2">
                 HTML tags allows you to define the structure of a website.
               </p>
               <div>
