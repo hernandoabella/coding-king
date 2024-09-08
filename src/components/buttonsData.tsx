e994522d9960b06e38f222854fd168d41399370d
Hernando Abella <hernandoabella@gmail.com>
2024-09-07 22:34:02 -0500

    Update buttonsData.tsx

--- src/components/buttonsData.tsx
+++ src/components/buttonsData.tsx
@@ -1,75 +1,84 @@
 export const buttonsData = [
-    {
-      category: "Web Development",
-      buttons: [
-        { label: "HTML", icon: "devicon-html5-plain", type: "HTML" },
-        { label: "CSS", icon: "devicon-css3-plain", type: "CSS" },
-        { label: "TailwindCSS", icon: "devicon-tailwindcss-plain", type: "TailwindCSS" },
-        {
-          label: "JavaScript",
-          icon: "devicon-javascript-plain",
-          type: "JavaScript",
-        },
-        {
-          label: "TypeScript",
-          icon: "devicon-typescript-plain",
-          type: "TypeScript",
-        },
-        { label: "React", icon: "devicon-react-plain", type: "React" },
-        { label: "Vue", icon: "devicon-vuejs-plain", type: "Vue" },
-        { label: "Angular", icon: "devicon-angular-plain", type: "Angular" },
-        { label: "Svelte", icon: "devicon-svelte-plain", type: "Svelte" },
-      ],
-    },
-    {
-      category: "Control Version",
-      buttons: [
-        { label: "Git", icon: "devicon-git-plain", type: "Git" },
-        { label: "Github", icon: "devicon-github-plain", type: "Github" },
-      ],
-    },
-    {
-      category: "Programming Languages",
-      buttons: [
-        { label: "Python", icon: "devicon-python-plain", type: "Python" },
-        { label: "Java", icon: "devicon-java-plain", type: "Java" },
-        { label: "C++", icon: "devicon-cplusplus-plain", type: "C++" },
-        { label: "C#", icon: "devicon-csharp-plain", type: "C#" },
-        { label: "Swift", icon: "devicon-swift-plain", type: "Swift" },
-        { label: "Kotlin", icon: "devicon-kotlin-plain", type: "Kotlin" },
-      ],
-    },
-    {
-      category: "Databases",
-      buttons: [
-        { label: "MySQL", icon: "devicon-mysql-plain", type: "Mysql" },
-        {
-          label: "PostgresSQL",
-          icon: "devicon-postgresql-plain",
-          type: "PostgresSQL",
-        },
-        { label: "MongoDB", icon: "devicon-mongodb-plain", type: "MongoDB" },
-        { label: "Redis", icon: "devicon-redis-plain", type: "Github" },
-      ],
-    },
-    {
-      category: "Cloud Computing",
-      buttons: [
-        { label: "AWS", icon: "devicon-amazonwebservices-plain", type: "AWS" },
-        { label: "Azure", icon: "devicon-azure-plain", type: "Azure" },
-        {
-          label: "Google Cloud",
-          icon: "devicon-googlecloud-plain",
-          type: "Google Cloud",
-        },
-      ],
-    },
-    {
-      category: "Mobile Development",
-      buttons: [
-        { label: "Android", icon: "devicon-android-plain", type: "Android" },
-        { label: "XCode", icon: "devicon-xcode-plain", type: "XCODE" },
-        { label: "Flutter", icon: "devicon-flutter-plain", type: "Flutter" },
-      ],
-    },
-  ];
+  {
+    category: "Web Development",
+    buttons: [
+      { label: "HTML", icon: "devicon-html5-plain", type: "HTML" },
+      { label: "CSS", icon: "devicon-css3-plain", type: "CSS" },
+      {
+        label: "Bootstrap",
+        icon: "devicon-bootstrap-plain",
+        type: "Bootstrap",
+      },
+      {
+        label: "TailwindCSS",
+        icon: "devicon-tailwindcss-plain",
+        type: "TailwindCSS",
+      },
+      {
+        label: "JavaScript",
+        icon: "devicon-javascript-plain",
+        type: "JavaScript",
+      },
+      {
+        label: "TypeScript",
+        icon: "devicon-typescript-plain",
+        type: "TypeScript",
+      },
+      { label: "React", icon: "devicon-react-plain", type: "React" },
+      { label: "Vue", icon: "devicon-vuejs-plain", type: "Vue" },
+      { label: "Angular", icon: "devicon-angular-plain", type: "Angular" },
+      { label: "Svelte", icon: "devicon-svelte-plain", type: "Svelte" },
+    ],
+  },
+  {
+    category: "Control Version",
+    buttons: [
+      { label: "Git", icon: "devicon-git-plain", type: "Git" },
+      { label: "Github", icon: "devicon-github-plain", type: "Github" },
+    ],
+  },
+  {
+    category: "Programming",
+    buttons: [
+      { label: "Python", icon: "devicon-python-plain", type: "Python" },
+      { label: "Java", icon: "devicon-java-plain", type: "Java" },
+      { label: "C++", icon: "devicon-cplusplus-plain", type: "C++" },
+      { label: "C#", icon: "devicon-csharp-plain", type: "C#" },
+      { label: "Swift", icon: "devicon-swift-plain", type: "Swift" },
+      { label: "Kotlin", icon: "devicon-kotlin-plain", type: "Kotlin" },
+    ],
+  },
+  {
+    category: "Databases",
+    buttons: [
+      { label: "MySQL", icon: "devicon-mysql-plain", type: "Mysql" },
+      {
+        label: "PostgresSQL",
+        icon: "devicon-postgresql-plain",
+        type: "PostgresSQL",
+      },
+      { label: "MongoDB", icon: "devicon-mongodb-plain", type: "MongoDB" },
+      { label: "Redis", icon: "devicon-redis-plain", type: "Github" },
+    ],
+  },
+  {
+    category: "Cloud Computing",
+    buttons: [
+      { label: "AWS", icon: "devicon-amazonwebservices-plain", type: "AWS" },
+      { label: "Azure", icon: "devicon-azure-plain", type: "Azure" },
+      {
+        label: "Google Cloud",
+        icon: "devicon-googlecloud-plain",
+        type: "Google Cloud",
+      },
+    ],
+  },
+  {
+    category: "Mobile Development",
+    buttons: [
+      { label: "Android", icon: "devicon-android-plain", type: "Android" },
+      { label: "XCode", icon: "devicon-xcode-plain", type: "XCODE" },
+      { label: "Flutter", icon: "devicon-flutter-plain", type: "Flutter" },
+    ],
+  },
+];
