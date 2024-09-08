9288d7eead05050577f86807a72cb6894913a39b
Hernando Abella <hernandoabella@gmail.com>
2024-09-07 22:34:10 -0500

    Create LeftSideBar.tsx

---
+++ src/components/LeftSideBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div
+      className="bg-white h-full fixed p-10 left-side-bar border-r"
+    >
+      Books
+    </div>
+  );
+};
+
+export default SideBar;
