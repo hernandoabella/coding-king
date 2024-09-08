80f59f9d6d8d731568cf3f8047cc803f8151ed57
Hernando Abella <hernandoabella@gmail.com>
2024-09-07 22:34:13 -0500

    Create RightSideBar.tsx

---
+++ src/components/RightSideBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const RightSideBar = () => {
+  return (
+    <div className='bg-white p-10 h-full right-side-bar border-l'>
+        <h3 className='font-bold'>Related content</h3>
+        <ul>
+            <li>Clean Code</li>
+            <li>...</li>
+        </ul>
+    </div>
+  )
+}
+
+export default RightSideBar
