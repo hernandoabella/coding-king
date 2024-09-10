2b92abc1695db08bec32750328c1fbca70311083
Hernando Abella <hernandoabella@gmail.com>
2024-09-09 22:05:59 -0500

    Update RightSideBar.tsx

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RightSideBar = () => {
   return (
-    <div className='bg-white p-10 h-full right-side-bar border-l'>
+    <div className=' p-10 h-full right-side-bar border-l fixed right-0'>
         <h3 className='font-bold'>Related content</h3>
         <ul>
             <li>Clean Code</li>
