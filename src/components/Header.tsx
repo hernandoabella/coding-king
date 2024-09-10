cebb25b7eeee2e9cb8cda1464929cd6cb4461391
Hernando Abella <hernandoabella@gmail.com>
2024-09-09 22:05:54 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,9 +19,9 @@ function Header() {
 
   return (
     <header className="w-full">
-      {/* <div className="m-5 w-full">
+      <div className="m-5 w-full">
         <div className="dropdown mb-5">
-          <div tabIndex={0} role="button" className="btn m-1 btn-outline">
+          <div tabIndex={0} role="button" className="btn btn-outline">
             Theme
             <svg
               width="12px"
@@ -94,11 +94,12 @@ function Header() {
             </li>
           </ul>
         </div>
-      </div> */}
+      </div>
       <div className="text-center p-5">
-        <h1 className="font-extrabold text-3xl">🤴codingking.net</h1>
+        <h1 className="font-extrabold text-3xl">🤴codingking</h1>
         <p>
-          Free <del>programming</del> <b>coding</b> resources to help you level up
+          Free <del>programming</del> <b>coding</b> resources to help you level
+          up
         </p>
       </div>
     </header>
