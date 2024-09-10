081edcde1b53bd267f87f50774c1d5dcd696c6b2
Hernando Abella <hernandoabella@gmail.com>
2024-09-09 22:05:56 -0500

    Update LeftSideBar.tsx

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const SideBar = () => {
   return (
     <div
-      className="bg-white h-full fixed p-10 left-side-bar border-r"
+      className=" h-full fixed p-10 left-side-bar border-r"
     >
       Books
     </div>
