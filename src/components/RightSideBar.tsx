f314e98a2d03e16e5f418df613e578a7f57ac31a
Hernando Abella <hernandoabella@gmail.com>
2024-09-27 16:21:25 -0500

    Update RightSideBar.tsx

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -9,52 +9,56 @@ const RightSideBar = () => {
       <h3 className="font-bold">Related content</h3>
       <ul className="pl-2">
         <li>
-          <a href="#" className="hover:underline text-sm hover:text-xl transition-all">
+          <a href="#" className="hover:underline text-sm">
             Clean Code
           </a>
         </li>
         <li>
-          <a href="#" className=" hover:underline text-sm hover:text-xl transition-all">
+          <a href="#" className=" hover:underline text-sm">
             Algorithms and Data Structures
           </a>
         </li>
         <li>
-          <a href="#" className=" hover:underline text-sm hover:text-xl transition-all">
+          <a href="#" className=" hover:underline text-sm">
             Object Oriented Programming
           </a>
         </li>
         <li>
-          <a href="#" className=" hover:underline text-sm hover:text-xl transition-all">
+          <a href="#" className=" hover:underline text-sm">
             Design Patterns
           </a>
         </li>
         <li>
-          <a href="#" className=" hover:underline text-sm hover:text-xl transition-all">
+          <a href="#" className=" hover:underline text-sm">
             SOLID
           </a>
         </li>
         <li>
-          <a href="#" className=" hover:underline text-sm hover:text-xl transition-all">
+          <a href="#" className=" hover:underline text-sm">
             Test Driven Data
           </a>
         </li>
         <li>
-          <a href="#" className=" hover:underline text-sm hover:text-xl transition-all">
+          <a href="#" className=" hover:underline text-sm">
             Cyber Security
           </a>
         </li>
         <li>
-          <a href="#" className=" hover:underline text-sm hover:text-xl transition-all">
+          <a href="#" className=" hover:underline text-sm">
             Artificial Inteligence
           </a>
         </li>
         <li>
-          <a href="#" className=" hover:underline text-sm hover:text-xl transition-all">
+          <a href="#" className=" hover:underline text-sm">
             Soft Skills
           </a>
         </li>
         <li>
-          <a href="https://github.com/hernandoabella/programming-terms-and-definitions" target="_blank" className=" hover:underline hover:text-xl transition-all text-sm">
+          <a
+            href="https://github.com/hernandoabella/programming-terms-and-definitions"
+            target="_blank"
+            className=" hover:underline text-sm"
+          >
             +200 Programming Terms and Definitions
           </a>
         </li>
