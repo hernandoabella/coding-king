d61c6137becbafd8877e19ebfa3a1e8ef86895e3
Hernando Abella <hernandoabella@gmail.com>
2025-01-23 14:37:06 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
     <div>
       <footer>
         <div className="p-10 text-center">
-          <p>&copy; Coding-King {new Date().getFullYear()}.</p>
+          <p>&copy; Coding King 2024 - {new Date().getFullYear()}.</p>
         </div>
       </footer>
     </div>
