3dd8376e8b53cb2d0652d4838553e932206c38a2
Hernando Abella <hernandoabella@gmail.com>
2025-02-25 15:17:30 -0500

    Update buttonsData.tsx

--- src/components/buttonsData.tsx
+++ src/components/buttonsData.tsx
@@ -1,47 +1,12 @@
 export const buttonsData = [
   {
-    category: "Web Development",
+    category: "Programming",
     buttons: [
-      { label: "HTML", icon: "devicon-html5-plain", type: "HTML" },
-      { label: "CSS", icon: "devicon-css3-plain", type: "CSS" },
-      {
-        label: "Bootstrap",
-        icon: "devicon-bootstrap-plain",
-        type: "Bootstrap",
-      },
-      {
-        label: "TailwindCSS",
-        icon: "devicon-tailwindcss-plain",
-        type: "TailwindCSS",
-      },
-      {
-        label: "JavaScript",
-        icon: "devicon-javascript-plain",
-        type: "JavaScript",
-      },
-      {
-        label: "JQuery",
-        icon: "devicon-jquery-plain",
-        type: "JQuery",
-      },
-      {
-        label: "TypeScript",
-        icon: "devicon-typescript-plain",
-        type: "TypeScript",
-      },
-      
+      { label: "Python", icon: "devicon-python-plain", type: "Python" },
+      { label: "JavaScript", icon: "devicon-javascript-plain", type: "JavaScript" },
+      { label: "Java", icon: "devicon-java-plain", type: "Java" },
+      { label: "C++", icon: "devicon-cplusplus-plain", type: "C++" },
+      { label: "C#", icon: "devicon-csharp-plain", type: "C#" },
     ],
   },
-  
-  // {
-  //   category: "Programming",
-  //   buttons: [
-  //     { label: "Python", icon: "devicon-python-plain", type: "Python" },
-  //     { label: "Java", icon: "devicon-java-plain", type: "Java" },
-  //     { label: "C++", icon: "devicon-cplusplus-plain", type: "C++" },
-  //     { label: "C#", icon: "devicon-csharp-plain", type: "C#" },
-  //     { label: "Swift", icon: "devicon-swift-plain", type: "Swift" },
-  //     { label: "Kotlin", icon: "devicon-kotlin-plain", type: "Kotlin" },
-  //   ],
-  // },
 ];
