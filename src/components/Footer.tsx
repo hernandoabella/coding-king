b2fbb2ce234e860b68f71a4da13e980fe26f28ae
Hernando Abella <hernandoabella@gmail.com>
2025-05-05 13:24:09 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="fixed right-20 top-1/2 -translate-y-1/2 rotate-90 origin-bottom-right">
+    <div className="absolute right-10 bottom-10 rotate-90 origin-bottom-right">
       <footer>
         <div className="bg-gray-900 text-white px-4 py-2 shadow-lg rounded-md">
           <p className="text-sm font-semibold">
