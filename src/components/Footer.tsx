58ac0e8f060f0287f26a407ff85cc8d474cfdde7
Hernando Abella <hernandoabella@gmail.com>
2025-05-05 01:03:27 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 function Footer() {
   return (
-    <div>
+    <div className="fixed right-20 top-1/2 -translate-y-1/2 rotate-90 origin-bottom-right">
       <footer>
-        <div className="p-10 text-center">
-          <p>&copy; Coding King {new Date().getFullYear()}.</p>
+        <div className="bg-gray-900 text-white px-4 py-2 shadow-lg rounded-md">
+          <p className="text-sm font-semibold">
+            &copy; Coding King {new Date().getFullYear()}
+          </p>
         </div>
       </footer>
     </div>
