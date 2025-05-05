9a3a4fba8dc6f5d4a94dce490fc1896a364cf1b7
Hernando Abella <hernandoabella@gmail.com>
2025-05-05 14:57:12 -0500

    Update ImportUI.tsx

--- src/components/ImportUI.tsx
+++ src/components/ImportUI.tsx
@@ -1,10 +1,10 @@
-import PHP from "./ui/PHP";
 import C from "./ui/C";
 import Cpp from "./ui/Cpp";
 import Csharp from "./ui/Csharp";
 import Java from "./ui/Java";
 import JavaScript from "./ui/JavaScript";
 import Python from "./ui/Python";
+import PHP from "./ui/PHP";
 
 export {
   Python,
@@ -12,5 +12,6 @@ export {
   Java, 
   C,
   Cpp,
-  Csharp
+  Csharp,
+  PHP
 };
