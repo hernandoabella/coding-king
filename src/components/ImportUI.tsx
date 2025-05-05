805aff0ac9275cad283c1a68003f5ab9c3054bdc
Hernando Abella <hernandoabella@gmail.com>
2025-05-05 17:52:13 -0500

    Update ImportUI.tsx

--- src/components/ImportUI.tsx
+++ src/components/ImportUI.tsx
@@ -5,13 +5,21 @@ import Java from "./ui/Java";
 import JavaScript from "./ui/JavaScript";
 import Python from "./ui/Python";
 import PHP from "./ui/PHP";
+import TypeScript from "./ui/TypeScript";
+import Go from "./ui/Go";
+import Kotlin from "./ui/Kotlin";
+import Rust from "./ui/Rust";
 
 export {
   Python,
   JavaScript,
+  TypeScript,
   Java, 
+  Go,
+  Kotlin,
   C,
   Cpp,
   Csharp,
-  PHP
+  PHP,
+  Rust
 };
